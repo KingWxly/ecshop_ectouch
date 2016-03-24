3fd4c0eb66c9b39b8ef294a7456cfd354b93cbb5
Clark <18638155414@163.com>
2016-03-24 14:35:58 +0800

    安装sql，初始化项目

--- mobile/data/config.php
+++ mobile/data/config.php
@@ -1,11 +1,11 @@
 <?php
 return array(
-    'DB_TYPE' => 'mysql',
-    'DB_HOST' => 'localhost',
-    'DB_NAME' => 'ectouch_db',
-    'DB_USER' => 'root',
-    'DB_PWD' => '',
-    'DB_PORT' => '3306',
-    'DB_PREFIX' => 'ecs_',
-    'DB_CHARSET' => 'utf8'
+    'DB_HOST'   => 'localhost',
+    'DB_USER'   => 'root',
+    'DB_PWD'   => '789456123Abc',
+    'DB_NAME'   => 'ec_dev',
+    'DB_PREFIX'   => 'ecs_',
+    'DB_PORT'   => '3306',
+    'DB_CHARSET'   => 'utf8',
+    'DB_TYPE'   => 'mysql',
 );
--- mobile/data/version.php
+++ mobile/data/version.php
@@ -2,4 +2,4 @@
 define('APPNAME', 'ECTouch');
 define('VERSION', '1.0');
 define('RELEASE', '20141218');
-define('ECTOUCH_AUTH_KEY', '');
+define('ECTOUCH_AUTH_KEY', '5DD03C0EF3FCDC91A8655661D2F07499');
